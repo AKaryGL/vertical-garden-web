0531313ff5de0f969bfdc8893d07299caaecbdb8
jesuswav <garciajesse2003@gmail.com>
2024-02-28 19:47:22 -0600

    first modification in the App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Que tal comadres</h1>
+      <h4>y Jaime...</h4>
+    </div>
+  );
+}
+
+export default App;
