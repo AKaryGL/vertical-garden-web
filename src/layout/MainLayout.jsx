27e73e60c573f6d8de12ff1135ab46c347437da7
jesuswav <garciajesse2003@gmail.com>
2024-03-14 23:32:09 -0600

    header ready

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../components/Header/Header'
+import './MainLayout.css'
 
 const MainLayout = ({ children }) => {
   return (
     <>
       <Header />
-      <div>{children}</div>
+      <div className='main-container'>{children}</div>
     </>
   )
 }
---
+++ src/pages/Historical.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Historical = () => {
+  return (
+    <div>
+      <h1>Historical page</h1>
+    </div>
+  )
+}
+
+export default Historical
